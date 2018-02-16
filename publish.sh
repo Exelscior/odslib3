b0693050c226cffcd57a04da8581e86e398f06c1
joshua.logan <joshua.logan@bosstek.net>
2018-02-16 17:21:57 +0100

    Fix README for pypi

--- publish.sh
+++ publish.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo Have you updated version?
-read
 ./setup.py sdist --formats=bztar upload
 echo "Generating docs"
 ./setup.py build_sphinx
 echo "Uploading docs to pypi"
 ./setup.py upload_docs --upload-dir build/sphinx/html/
-
